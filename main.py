b7d773b5d649057a7a38f59a625547c8a1bbafab
Poliana <polianasamonte@gmail.com>
2025-05-16 07:03:35 -0300

    indice invertido por frequencia e por ausência e presença pronto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import re
 from collections import Counter
+from itertools import islice
 from time import sleep
 
 #palavrasParaRetirar
@@ -12,14 +13,43 @@ stopWords = [
     "nosso", "nossa", "nossos", "nossas",
     "esse", "essa", "isso", "este", "esta", "isto",
     "aquele", "aquela", "aquilo",
-    "quem", "que", "qual", "sim", "não", "com", "tem"
+    "quem", "que", "qual", "sim", "não", "com", "tem", "yes", "not"
 ]
 
-def index_collection():
-    #first step: to index the collection we need to read it
-    print('Método para indexar a coleção')
+def index_collection(vocabulary):
 
+    collection = create_collection_dictionary()
 
+    # index = list() [0 = para, 1 = que, 2 = arquivos, ... etc.]
+    # aux = 0
+    index = dict()
+
+    for vocabulary_key, total_freq in vocabulary.items():
+        index[vocabulary_key] = [total_freq,
+                                 {}]  # começa com uma lista contendo a frequência total e um dicionário vazio
+
+        for doc_name, terms in collection.items():
+            if vocabulary_key in terms:
+                freq = terms[vocabulary_key]
+                index[vocabulary_key][1][doc_name] = freq  # adiciona ao dicionário de documentos
+
+       # print(index[vocabulary_key])
+    return index
+
+def bool_index(vocabulary):
+    collection = create_collection_dictionary()
+    #print(collection)
+    bool = dict()
+
+    for word, total_freq in vocabulary.items():
+        bool[word] = {}
+        for doc_name, doc_value in collection.items():
+            if word in doc_value:
+                bool[word][doc_name] = 1;
+            else:
+                bool[word][doc_name] = 0;
+
+    print(bool)
 #Lê o arquivo e extrai o vocabulário colocando em um array.
 # O vocabulário é toda palavra com mais de 2 caracteres que não sejam stopwords  (pronomes, caracteres especiais, não, sim, e etc.)
 def extract_vocabulary(fileName):
@@ -51,10 +81,32 @@ def create_collection_dictionary():
 
     for i in range(1, qtd_files + 1): #o range é exclusivo no final, entao para chegar até o 2 precisa adicionar mais um. Ex, se for range(1,5) ele vai do 1 ao 4
         dictionary['doc'+str(i)+'.txt'] = create_dictionary('doc'+str(i)+'.txt')
-    print(dictionary)
+
+    return dictionary
+    #print(dictionary)
     #para ver o dicionario formatado jogue o resultado do terminal nesse link: https://jsonformatter.curiousconcept.com/#
 
 #def indexar(arrayComPalavrasRepetidas):
+
+def get_most_relevant_terms():
+
+    dictionary = create_collection_dictionary()
+    vocabulary = dict()
+
+    for key, value in dictionary.items():
+        for key, v in value.items():
+            if key in vocabulary:
+                vocabulary[key] = vocabulary[key] + v
+            else:
+                vocabulary[key] = v
+   # print(vocabulary)
+    sorted_vocabulary = dict(sorted(vocabulary.items(),key=lambda word: word[1], reverse=True))
+
+    vocabulary_50 = dict(islice(sorted_vocabulary.items(), 20))
+
+
+    #print(vocabulary_50)
+    return vocabulary_50
 
 def menu():
 
@@ -94,8 +146,10 @@ def menu():
 if __name__ == "__main__":
     # menu()
     #vocabulary = extract_vocabulary('doc1.txt')
-    dictionary = create_collection_dictionary()
+    result = get_most_relevant_terms() #menu 2
+    #print(result)
 
+    index = bool_index(result)
 
     '''
     vocabulary2 = create_dictionary('doc2.txt')
